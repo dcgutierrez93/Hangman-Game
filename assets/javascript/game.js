a8e266cc7679eed6453b064b22b83f9a95528ecf
dcgutierrez93 <dcgutierrez93@yahoo.com>
2017-11-04 13:18:10 -0500

    added to javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,234 @@
-<!DOCTYPE html>
-<html lang="en-us">
-  <head>
-    <meta charset="UTF-8">
-    <title>My Hangman Game</title>
-    <link rel="stylesheet" href="./assets/css/style.css">
-    <link rel="stylesheet" href="nice80sHeader.css">
+// =============================================================================
 
-  </head>
-  <body>
-    <div id="wrapper">
-      <h1 class="chrome">HANGMAN</h1>
-      <h3 class="dreams">80s style</h3>
-    </div>
+// Create Object
 
-    <div id="container">
-      <h2 id="music"></h2>
-      <!-- The div where our band image will be shown. -->
-      <div id="band-div"><img src="images/hangman-boardgame.jpg" alt="hangman board game from 80s"></div>
-      <div id="game-section">
-        <p>Press any key to get started!</p>
-        <div>
-          <h3 class="section-title">Wins</h3>
-          <!-- The div where our wins will be shown. -->
-          <div id="wins"></div>
-        </div>
-        <div id="current-word-section">
-          <h3 class="section-title">Current Word</h3>
-          <!-- The div where our current word (and guessing progress) will be shown. -->
-          <div id="current-word"></div>
-        </div>
-        <div id="guesses-remaining-section">
-          <h3 class="section-title">Number of Guesses Remaining</h3>
-          <!-- The div where our remaining guesses will be shown. -->
-          <div id="guesses-remaining"></div>
-        </div>
-        <div id="guessed-letters-section">
-          <h3 class="section-title">Letters Already Guessed</h3>
-          <!-- The div where our guesses letters will be shown. -->
-          <div id="guessed-letters"></div>
-        </div>
-      </div>
-    </div>
+// =============================================================================
+var hangmanGame = {
+    // Object of words to be chosen
+    wordsToPick: {
+        genesis: {
+            picture: "genesis.jpg",
+            song   : "Illegal Alien",
+            preview: "https://p.scdn.co/mp3-preview/b29a2b925b9654e0efaaff37504fd234307e0ad8"
+        },
+        madonna: {
+            picture: "madonna.jpg",
+            song   : "Material Girl",
+            preview: "https://p.scdn.co/mp3-preview/5ff7f7b7d2af1a747da275bed3c49054c01b5b1c"
+        },
+        toto: {
+            picture: "toto.jpg",
+            song   : "Rosanna",
+            preview: "https://p.scdn.co/mp3-preview/7cef811eaeb7c7b98245750e73d9d68e9008f317"
+        },
+        queen: {
+            picture: "queen.jpg",
+            song   : "Princes of the Universe",
+            preview: "https://p.scdn.co/mp3-preview/b84f24300476f3d3f20056d2388cc51db2e3891f"
+        },
+        u2: {
+            picture: "u2.jpg",
+            song   : "With or Without You",
+            preview: "https://p.scdn.co/mp3-preview/28365dff1890075c1371628371cd0e5bbff9a3a3"
+        },
+        metallica: {
+            picture: "metallica.jpg",
+            song   : "Master of Puppets",
+            preview: "https://p.scdn.co/mp3-preview/60e6f8dab43f176dd9fb5e795d4e6459bad52e9e"
+        },
+        journey: {
+            picture: "journey.jpg",
+            song   : "Don't Stop Believin'",
+            preview: "https://p.scdn.co/mp3-preview/21b9abd3cd2eea634e17a917196fdd5ba2e82670"
+        },
+        inxs: {
+            picture: "inxs.jpg",
+            song   : "Need You Tonight",
+            preview: "https://p.scdn.co/mp3-preview/61b17a335d5afc1c4086b1b08e2400f0da147977"
+        },
+        poison: {
+            picture: "poison.jpg",
+            song   : "Fallen Angel",
+            preview: "https://p.scdn.co/mp3-preview/0365ad1f152f1834b42b857c4625191cebf9f987"
+        },
+        rush: {
+            picture: "rush.jpg",
+            song   : "Limelight",
+            preview: "https://p.scdn.co/mp3-preview/154987dfb07f2fc5ed7aa4d76b80c5dc08ff4d6b"
+        },
+        blondie: {
+            picture: "blondie.jpg",
+            song   : "Call Me",
+            preview: "https://p.scdn.co/mp3-preview/ed5a443bc86176135ebca8a114f66f4d814d4c90"
+        }
+    },
 
-    <!-- Linking to our JavaScript file. -->
-    <script type="text/javascript" src="hangmanGame.js"></script>
-  </body>
+    // Variables that set the initial state of our hangman game.
+    wordInPlay      : null,
+    lettersOfTheWord: [],
+    matchedLetters  : [],
+    guessedLetters  : [],
+    guessesLeft     : 0,
+    totalGuesses    : 0,
+    letterGuessed   : null,
+    wins            : 0,
+
+    // The setupGame method is called when the page first loads.
+    setupGame: function() {
+        // Here we pick a random word.
+        var objKeys = Object.keys(this.wordsToPick);
+        this.wordInPlay = objKeys[Math.floor(Math.random() * objKeys.length)];
+
+        this.lettersOfTheWord = this.wordInPlay.split("");
+        this.rebuildWordView();
+        this.processUpdateTotalGuesses();
+    },
+
+    // This function is run whenever the user guesses a letter..
+    updatePage: function(letter) {
+        // If the user has no guesses left, restart the game.
+        if (this.guessesLeft === 0) {
+            this.restartGame(// Otherwise...);
+        } else {
+            // Check for and handle incorrect guesses.
+            this.updateGuesses(letter);
+
+            // Check for and handle correct guesses.
+            this.updateMatchedLetters(letter);
+
+            // Rebuild the view of the word. Guessed letters are revealed, unguessed letters have a "_".
+            this.rebuildWordView();
+
+            // If the user wins, restart the game.
+            if (this.updateWins() === true) {
+                this.restartGame();
+            }
+        }
+
+    },
+
+    // This function governs what happens when the user makes an incorrect guess (that they haven't guessed before).
+    updateGuesses: function(letter) {
+        // If the letter is not in the guessedLetters array, and the letter is not in the lettersOfTheWord array..
+        if ((this.guessedLetters.indexOf(letter) === -1) && (this.lettersOfTheWord.indexOf(letter) === -1)) {
+
+            // Add the letter to the guessedLetters array.
+            this.guessedLetters.push(letter);
+
+            // Decrease guesses by one.
+            this.guessesLeft--;
+
+            // Update guesses remaining and guesses letters on the page.
+            document.querySelector("#guesses-remaining").innerHTML = this.guessesLeft;
+            document.querySelector("#guessed-letters").innerHTML = this.guessedLetters.join(", ");
+        }
+    },
+
+    // This function sets the initial guesses the user gets.
+    processUpdateTotalGuesses: function() {
+        // The user will get more guesses the longer the word is.
+        this.totalGuesses = this.lettersOfTheWord.length + 5;
+        this.guessesLeft = this.totalGuesses;
+
+        // Render the guesses left to the page.
+        document.querySelector("#guesses-remaining").innerHTML = this.guessesLeft;
+    },
+
+    // This function governs what happens if the user makes a successful guess.
+    updateMatchedLetters: function(letter) {
+        // Loop through the letters of the "solution".
+        for (var i = 0; i < this.lettersOfTheWord.length; i++) {
+            // If the guessed letter is in the solution, and we haven't guessed it already..
+            if ((letter === this.lettersOfTheWord[i]) && (this.matchedLetters.indexOf(letter) === -1)) {
+                // Push the newly guessed letter into the matchedLetters array.
+                this.matchedLetters.push(letter);
+            }
+        }
+    },
+
+    // This function builds the display of the word that is currently being guessed.
+    rebuildWordView: function() {
+        // We start with an empty string.
+        var wordView = "";
+
+        // Loop through the letters of the word we are trying to guess..
+        for (var i = 0; i < this.lettersOfTheWord.length; i++) {
+            // If the current letter has been guessed, display that letter.
+            if (this.matchedLetters.indexOf(this.lettersOfTheWord[i]) !== -1) {
+                wordView += this.lettersOfTheWord[
+                    i// If it hasn't been guessed, display a "_" instead.
+                ];
+            } else {
+                wordView += "&nbsp;_&nbsp;";
+            }
+        }
+
+        // Update the page with the new string we built.
+        document.querySelector("#current-word").innerHTML = wordView;
+    },
+
+    // Function that "restarts" the game by resetting all of the variables.
+    restartGame: function() {
+        document.querySelector("#guessed-letters").innerHTML = "";
+        this.wordInPlay = null;
+        this.lettersOfTheWord = [];
+        this.matchedLetters = [];
+        this.guessedLetters = [];
+        this.guessesLeft = 0;
+        this.totalGuesses = 0;
+        this.letterGuessed = null;
+        this.setupGame();
+        this.rebuildWordView();
+    },
+
+    // Function that checks to see if the user has won.
+    updateWins: function() {
+        var win;
+        if (this.matchedLetters.length === 0) {
+            win = false;
+        } else {
+            win = true;
+        }
+        for (var i = 0; i < this.lettersOfTheWord.length; i++) {
+            if (this.matchedLetters.indexOf(this.lettersOfTheWord[i]) === -1) {
+                win = false;
+            }
+        }
+
+        // If win is true...
+        if (win) {
+
+            // Increment wins.
+            this.wins = this.wins + 1;
+
+            // Update wins on the page.
+            document.querySelector("#wins").innerHTML = this.wins;
+
+            // Update the song title and band on the page.
+            document.querySelector("#music").innerHTML = this.wordsToPick[this.wordInPlay].song + " By " + this.wordInPlay;
+
+            // Update the image of the band on the page.
+            document.querySelector("#bandDiv").innerHTML = "<img class='band-image' src='images/" + this.wordsToPick[this.wordInPlay].picture + "' alt='" + this.wordsToPick[this.wordInPlay].song + "'>";
+
+            // Play an audio track of the band.
+            var audio = new Audio(this.wordsToPick[this.wordInPlay].preview);
+            audio.play();
+
+            // return true, which will trigger the restart of our game in the updatePage function.
+            return true;
+        }
+        // If win is false, return false to the updatePage function. The game goes on!
+        return false;
+    }
+};
+
+// Initialize the game when the page loads.
+hangmanGame.setupGame();
+
+// When a key is pressed..
+document.onkeyup = function(event) {
+    // Capture pressed key and make it lowercase.
+    hangmanGame.letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+    // Pass the guessed letter into our updatePage function to run the game logic.
+    hangmanGame.updatePage(hangmanGame.letterGuessed);
+};
